d57c35d0b6a278c6bfb30fe1d71c5c7a396048a6
Mantu <mantu.kumar@itechnolabs.tech>
2025-01-28 16:22:51 +0530

    chore: code cleanup and example setup

--- src/app/(tabs)/explore.tsx
+++ src/app/(tabs)/explore.tsx
@@ -1,21 +1,25 @@
-import React from "react";
-import { Text, View } from "react-native";
-import { StyleSheet, } from "react-native-unistyles";
+import { HelloWave } from '@/components/HelloWave';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native-unistyles';
+import { UnistylesTheme } from 'react-native-unistyles/lib/typescript/src/types';
 
 const TabTwoScreen = () => {
-
   return (
     <View style={styles.container}>
       <Text>TabTwoScreen</Text>
+      <HelloWave />
     </View>
   );
 };
 
 export default TabTwoScreen;
 
-const styles = StyleSheet.create((theme) => ({
-  container:{
-    flex:1,
-    backgroundColor:theme.colors.white
-  }
+const styles = StyleSheet.create((theme: UnistylesTheme) => ({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.white,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 }));
